4340cdd50316ac90903f8d68af923f1dfc5b0ae5
katherine109 <avila13yamileth@gmail.com>
2024-08-13 17:45:25 -0600

    subiendo mis cambios

--- app/src/main/java/com/example/desafio1/Calculadora.kt
+++ app/src/main/java/com/example/desafio1/Calculadora.kt
@@ -15,6 +15,45 @@ class Calculadora : AppCompatActivity() {
         binding = ActivityCalculadoraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonSumar.setOnClickListener { calculate(Operation.ADD) }
+        binding.buttonRestar.setOnClickListener { calculate(Operation.SUBTRACT) }
+        binding.buttonMultiplicar.setOnClickListener { calculate(Operation.MULTIPLY) }
+        binding.buttonDividir.setOnClickListener { calculate(Operation.DIVIDE) }
+        binding.btnExit.setOnClickListener { finish() }
+
 
     }
+
+    private fun calculate(operation: Operation) {
+        val input1Text = binding.input1.text.toString()
+        val input2Text = binding.input2.text.toString()
+
+        if (input1Text.isEmpty() || input2Text.isEmpty()) {
+            Toast.makeText(this, "Por favor, ingresa ambos números", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val num1 = input1Text.toDouble()
+        val num2 = input2Text.toDouble()
+        var result = 0.0
+
+        when (operation) {
+            Operation.ADD -> result = num1 + num2
+            Operation.SUBTRACT -> result = num1 - num2
+            Operation.MULTIPLY -> result = num1 * num2
+            Operation.DIVIDE -> {
+
+                if (num2 == 0.0) {
+                    Toast.makeText(this, "No se puede dividir por cero", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                result = num1 / num2
+            }
+        }
+        binding.textViewResult.text = "Resultado: $result"
+    }
+
+    enum class Operation {
+        ADD, SUBTRACT, MULTIPLY,DIVIDE
+        }
 }
